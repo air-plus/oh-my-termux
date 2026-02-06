cc97edd2859d5f69e1380dac585e4cff517ad6a1
air-plus <jyin1221@gmail.com>
2026-02-06 13:02:14 +0800

    first commit

--- zsh/.config/zsh/funcs.sh
+++ zsh/.config/zsh/funcs.sh
@@ -1,9 +1,9 @@
 fgff() {
   local query
-  query=$(rg --files 2>/dev/null | fzf --height=50% --board --prompt="🔍 Search for: " \
+  query=$(rg --files 2>/dev/null | fzf --height=50% --border --prompt="🔍 Search for: " \
     --preview "echo {}" \
     --bind "change:reload:rg --color=always {q} ./{} || true" \
-    --preview-window=down:50%:warp)
+    --preview-window=down:50%:wrap)
 
   [[ -n "$query" ]] && print -S "$query" && echo "$query"
 }
