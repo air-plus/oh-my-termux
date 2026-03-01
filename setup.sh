e2477de884b46edd69bfb6baba5860ba3d4767eb
air-plus <jyin1221@gmail.com>
2026-03-01 12:56:55 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git clone git@github.com:air-plus/dotfiles.git
+cd $HOME/dotfiles
+chmod +x $HOME/dotfiles/setup.sh
+./setup.sh
 stow -vt $HOME */ --adopt
 bat cache --build
 mkdir -p $HOME/projects && touch $HOME/.hushlogin
