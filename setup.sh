a0af74f80c0ccb7a66c8c4db138c3a5be433cb99
air-plus <jyin1221@gmail.com>
2026-03-15 14:04:02 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-pkg install -y termux-api fish git-delta gh fastfetch starship eza zoxide bat fd ripgrep fzf jq htop yazi file stow lazygit helix helix-grammars zellij build-essential nodejs-lts
+exec 1>/dev/null
+pkg install -y termux-api fish git-delta fastfetch starship eza zoxide bat fd ripgrep fzf jq htop yazi file stow lazygit helix helix-grammars zellij build-essential nodejs-lts
 stow -t $HOME */ --adopt
 bat cache --build
 mkdir -p $HOME/projects && touch $HOME/.hushlogin
