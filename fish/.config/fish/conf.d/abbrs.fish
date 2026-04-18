5df41048d7aac5b7d0a30520c00a2d5f96512baa
air-plus <jyin1221@gmail.com>
2026-04-18 11:33:21 +0800

    first commit

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # eza 配置
-    alias ls='eza --icons --group-directories-first --color=always --time-style=long-iso'
-    abbr -a tree 'eza --tree --icons -I "node_modules|.git|target|vendor|dist|.cache|__pycache__"'
+    alias ls='eza --icons'
+    abbr -a tree 'eza --icons --tree -I "node_modules|.git|target|vendor|dist|.cache|__pycache__"'
 end
