ad576141f2e8debc1a0866135e574e0e0dd7e1d0
air-plus <jyin1221@gmail.com>
2026-04-18 08:24:23 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+git config --global --unset user.signingkey && git config --global --unset commit.gpgsign
 pkg install -y termux-api git fish git-delta fastfetch eza zoxide bat fd ripgrep starship fzf jq htop yazi file stow lazygit neovim zellij build-essential nodejs-lts tur-repo glibc-repo
 stow -t $HOME */ --adopt
 bat cache --build
