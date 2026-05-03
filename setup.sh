1c2efe5b1a9166cedfd2c95a989f9c9d9cdaf35c
air-plus <jyin1221@gmail.com>
2026-05-03 18:35:04 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -30,30 +30,30 @@ clear
 info '✨ 欢迎使用 Oh My Termux'
 info '↳ ⚡ 一个终极 Termux 配置'
 
-if [ -z "${TERMUX_VERSION:-}" ]; then
+if [[ -z ${TERMUX_VERSION:-} ]]; then
   error '当前环境不是 Termux'
 fi
 
 info '📥 正在添加 TUR'
-apt install -yqq tur-repo || error 'TUR 添加失败'
+apt install -y tur-repo &>/dev/null || error 'TUR 添加失败'
 
 info '📥 正在安装基本工具包（此过程耗时较长）'
-apt install -yqq \
-  termux-api git fish git-delta fastfetch \
-  eza zoxide bat fd ripgrep \
-  starship fzf jq htop yazi \
-  duf dust file stow lazygit \
-  neovim zellij build-essential nodejs-lts ||
-  error '基本工具包安装失败'
+apt install -y \
+  termux-api fish git-delta fastfetch eza \
+  zoxide bat fd ripgrep starship \
+  fzf jq htop yazi duf \
+  dust file stow lazygit neovim \
+  zellij build-essential nodejs-lts \
+  &>/dev/null || error '基本工具包安装失败'
 
 info '🔗 正在建立配置文件软链接'
-stow -vt "$HOME" */ --adopt || error '配置文件软链接建立失败'
+stow --adopt --verbose=0 -t "$HOME" */ || error '配置文件软链接建立失败'
 
 info '📦 正在构建 Bat 缓存'
 bat cache --build || error 'Bat 缓存构建失败'
 
 # ZeroTermux 环境处理
-if [ -d "$HOME/ZtInfo" ]; then
+if [[ -d $HOME/ZtInfo ]]; then
   mkdir -p "$HOME/.img"
 
   info '🗺️ 正在获取背景图片'
