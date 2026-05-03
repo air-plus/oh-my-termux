052063f93e1765ef37f134b5e6b6065d65e3b488
air-plus <jyin1221@gmail.com>
2026-05-03 17:30:12 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# 设置颜色
+# --- 颜色定义 ---
 COLOR_OFF='' # 无色
 
 RED='' # 红色
@@ -14,8 +14,10 @@ if [[ -t 1 ]]; then
   DIM='\033[0;2m'  # 灰色
 fi
 
+# --- 函数定义 ---
+
 error() {
-  echo -e "${RED}错误：${COLOR_OFF}" "$@" >&2
+  echo -e "${RED}↳ ❌ 错误：${COLOR_OFF}" "$@" >&2
   exit 1
 }
 
@@ -23,14 +25,19 @@ info() {
   echo -e "${DIM}$@ ${COLOR_OFF}"
 }
 
+clear
+
+info '✨ 欢迎使用 Oh My Termux'
+info '↳ ⚡ 一个终极 Termux 配置'
+
 if [ -z "$TERMUX_VERSION" ]; then
   error '当前环境不是 Termux'
 fi
 
-info '正在添加 TUR'
+info '📥 正在添加 TUR'
 apt install -yqq tur-repo || error 'TUR 添加失败'
 
-info '正在安装基本工具包（此过程耗时较长）'
+info '📥 正在安装基本工具包（此过程耗时较长）'
 apt install -yqq \
   termux-api git fish git-delta fastfetch \
   eza zoxide bat fd ripgrep \
@@ -39,23 +46,23 @@ apt install -yqq \
   neovim zellij build-essential nodejs-lts \
   >/dev/null 2>&1 || error '基本工具包安装失败'
 
-info '正在建立配置文件软链接'
+info '🔗 正在建立配置文件软链接'
 stow -vt "$HOME" */ --adopt || error '配置文件软链接建立失败'
 
-info '正在构建 Bat 缓存'
+info '📦 正在构建 Bat 缓存'
 bat cache --build || error 'Bat 缓存构建失败'
 
-# 检查是否为 ZeroTermux 环境
+# ZeroTermux 环境处理
 if [ -d "$HOME/ZtInfo" ]; then
   mkdir -p "$HOME/.img"
 
-  info '正在获取背景图片'
+  info '🗺️ 正在获取背景图片'
   curl -fsSLo "$HOME/.img/back.jpg" \
     https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/main/os/android-black-4k.png ||
     error '无法获取背景图片'
 fi
 
-info '正在切换默认 Shell'
+info '🐚 正在切换默认 Shell'
 chsh -s fish
 
-info 'Oh My Termux 安装完成'
+info '✨ Oh My Termux 安装完成'
