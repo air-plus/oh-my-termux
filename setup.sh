04e7d3b77f5aad670fd9a0b82a3914d4f24d5310
air-plus <jyin1221@gmail.com>
2026-05-03 18:54:18 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -28,7 +28,8 @@ info() {
 clear
 
 info '✨ 欢迎使用 Oh My Termux'
-info '↳ ⚡ 一个终极 Termux 配置'
+info '↳ ⚡ 终极 Termux 配置'
+echo
 
 if [[ -z ${TERMUX_VERSION:-} ]]; then
   error '当前环境不是 Termux'
@@ -37,7 +38,7 @@ fi
 info '📥 正在添加 TUR'
 apt install -y tur-repo &>/dev/null || error 'TUR 添加失败'
 
-info '📥 正在安装基本工具包（此过程耗时较长）'
+info '↓\n📥 正在安装基本工具包（此过程耗时较长）'
 apt install -y \
   termux-api fish git-delta fastfetch eza \
   zoxide bat fd ripgrep starship \
@@ -46,23 +47,23 @@ apt install -y \
   zellij build-essential nodejs-lts \
   &>/dev/null || error '基本工具包安装失败'
 
-info '🔗 正在建立配置文件软链接'
+info '↓\n🔗 正在建立配置文件软链接'
 stow --adopt --verbose=0 -t "$HOME" */ || error '配置文件软链接建立失败'
 
-info '📦 正在构建 Bat 缓存'
-bat cache --build || error 'Bat 缓存构建失败'
+info '↓\n📦 正在构建 Bat 缓存'
+bat cache --build &>/dev/null || error 'Bat 缓存构建失败'
 
 # ZeroTermux 环境处理
 if [[ -d $HOME/ZtInfo ]]; then
   mkdir -p "$HOME/.img"
 
-  info '🗺️ 正在获取背景图片'
+  info '↓\n🗺️ 正在获取背景图片'
   curl -fsSLo "$HOME/.img/back.jpg" \
     https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/main/os/android-black-4k.png ||
     error '无法获取背景图片'
 fi
 
-info '🐚 正在切换默认 Shell'
+info '↓\n🐚 正在切换默认 Shell'
 chsh -s fish
 
-info '✨ Oh My Termux 安装完成'
+info '↓\n✨ Oh My Termux 安装完成'
