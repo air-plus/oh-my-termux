bd306138a8e2e9a6e0c7d3627788c80ca3d987ac
air-plus <jyin1221@gmail.com>
2026-05-10 07:22:12 +0800

    first commit

--- fish/.config/fish/functions/tree.fish
+++ fish/.config/fish/functions/tree.fish
@@ -1,3 +1,3 @@
 function tree --description 由eza实现的tree
-    eza --icons --tree -I 'node_modules|.git|target|vendor|dist|.cache|__pycache__' $argv
+    eza --icons -TI 'node_modules|.git|target|vendor|dist|.cache|__pycache__' $argv
 end
