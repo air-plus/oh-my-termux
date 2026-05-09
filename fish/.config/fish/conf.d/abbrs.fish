14cc4057afa53d4578789cbfc5d4e2aee92901c4
air-plus <jyin1221@gmail.com>
2026-05-10 07:20:02 +0800

    first commit

--- fish/.config/fish/conf.d/abbrs.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    # eza 配置
-    alias ls='eza --icons'
-    abbr -a tree 'eza --icons --tree -I "node_modules|.git|target|vendor|dist|.cache|__pycache__"'
-end
---
+++ fish/.config/fish/functions/ls.fish
@@ -0,0 +1,3 @@
+function ls --description 由eza实现的ls
+    eza --icons $argv
+end
