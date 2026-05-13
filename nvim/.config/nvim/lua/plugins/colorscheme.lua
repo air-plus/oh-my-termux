3ceaf1c20cc2507f3a874d3d67a4f374ec6fc83d
air-plus <jyin1221@gmail.com>
2026-05-14 06:08:43 +0800

    first commit

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
+  -- 安装 Gruvbox 主题
   { "ellisonleao/gruvbox.nvim" },
 
+  -- 配置 LazyVim
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox", --使用 Gruvbox 主题
     },
   },
 }
