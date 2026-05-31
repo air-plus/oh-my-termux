9c943a08d4de04989b2a79b034c7868cd3646519
air-plus <jyin1221@gmail.com>
2026-05-31 16:57:47 +0800

    first commit

--- x-cmd/.x-cmd.root/local/cfg/websrc/region.setup.sh
+++ x-cmd/.x-cmd.root/local/cfg/websrc/region.setup.sh
@@ -1,3 +1,4 @@
-___X_CMD_WEBSRC_REGION=cn
-___X_CMD_WEBSRC_LIST='https://gitee.com/%s/%s/raw/%s/%s
+___X_CMD_WEBSRC_REGION=internet
+___X_CMD_WEBSRC_LIST='https://raw.githubusercontent.com/%s/%s/%s/%s
+https://gitee.com/%s/%s/raw/%s/%s
 '
