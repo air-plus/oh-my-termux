09b9f73461aac3665e0857cb924de9347b91d2c4
air-plus <jyin1221@gmail.com>
2026-06-01 19:26:00 +0800

    first commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 test ! -e "$HOME/.x-cmd.root/local/data/fish/rc.fish" || source "$HOME/.x-cmd.root/local/data/fish/rc.fish" # boot up x-cmd.
+
 if status is-interactive
+    fish_vi_key_bindings # 启用 Vi 模式键映射
+
     zoxide init fish | source # Zoxide 集成
 
     fastfetch -c examples/10.jsonc # 开屏 fastfetch
