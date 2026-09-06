eb1d2164fd9673370e957b0aaaea0976db1221b1
air-plus <jyin1221@gmail.com>
2026-09-06 19:10:50 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -7,21 +7,14 @@ set -euo pipefail
 # --- 函数定义 ---
 show_help() {
   cat <<EOF
-setup.sh
-Oh My Termux 安装引导
-========
+setup.sh - Oh My Termux 安装引导
+
 使用: $0 [-a] [-m MODULE_NAME]
 
 选项:
   -h, --help                  显示此引导信息
   -a, --all                   安装全部模块（默认行为）
   -m, --module MODULE_NAME    安装 MODULE_NAME
-
-示例：
-  ./setup.sh
-  ./setup.sh -a
-  ./setup.sh -m zsh
-  ./setup.sh -m zsh,nvim
 EOF
   exit 0
 }
